e43cdc5f0327daa4dd5b3196f1f9908451fc1265
aqmaria <aqmaria11@gmail.com>
2017-07-01 22:52:35 -0300

    Update pila.c

--- Algoritmos II/pila.c
+++ Algoritmos II/pila.c
@@ -40,28 +40,25 @@ bool pila_redimensionar(pila_t* pila, size_t nueva_capacidad )
 pila_t* pila_crear( void ) 
 {
 	pila_t* pila = malloc( sizeof( pila_t ) );
-	if( !pila ) return NULL;
-	pila->datos = (void**)malloc( TAM_INICIAL * sizeof( void* ) );
-	if ( !pila->datos ){
-		free(pila)
+	if( pila == NULL )
 		return NULL;
-	}
-	pila->cantidad = 0;
-	pila->capacidad = TAM_INICIAL;
-	return pila;
+    pila->datos = (void**)malloc( TAM_INICIAL * sizeof( void* ) );
+    if (pila->datos== NULL) 
+		return NULL;
+    pila->cantidad = 0;
+    pila->capacidad = TAM_INICIAL;
+    return pila;
 } 
 
 void pila_destruir( pila_t* pila )
 {  
-	if( pila ) {
-		free( pila->datos );
-		free( pila );
-	}
+	free( pila->datos );
+	free( pila );
 }
 
 bool pila_apilar( pila_t* pila , void* valor )
 {
-    if( pila->cantidad > pila->capacidad )
+    if( pila->cantidad >= pila->capacidad )
     {
 		if(! pila_redimensionar( pila, ( pila->capacidad) * 2 ) )
 			return false;
@@ -75,14 +72,16 @@ bool pila_esta_vacia( const pila_t* pila ){
 }
 
 void* pila_ver_tope( const pila_t* pila ){
-   return( pila->datos[ pila->cantidad -1 ] );
+	if(pila_esta_vacia(pila)) return NULL;
+   	return( pila->datos[ pila->cantidad -1 ] );
 }
 
 void* pila_desapilar( pila_t* pila )
 {
+	if( pila_esta_vacia(pila))
+		return NULL;
 	if( pila->cantidad < ( pila->capacidad/4 ) )
 		pila_redimensionar( pila, pila->capacidad / 2 );
 	pila->cantidad--;
 	return( pila->datos[ pila->cantidad ] );
 }
-
